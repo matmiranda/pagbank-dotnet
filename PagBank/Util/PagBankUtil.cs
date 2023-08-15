66cb5945bb5befda8ea04c00b7103a1cca99d0b7
matmiranda <mat.miranda@outlook.com>
2023-08-14 23:00:52 -0300

    melhoria cobertura de código

--- PagBank/Util/PagBankUtil.cs
+++ PagBank/Util/PagBankUtil.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography;
-
-namespace PagBank
+﻿namespace PagBank
 {
     public class PagBankUtil
     {
@@ -10,24 +8,6 @@ namespace PagBank
             var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
             var version = attribute?.InformationalVersion;
             return $"pagbank-dotnet/{version}";
-        }
-
-        internal static (string publicKey, string privateKey) CreatePrivatePublicKeys()
-        {
-            // Gere um par de chaves (chave pública e privada)
-            using var rsa = RSA.Create();
-            // Tamanho da chave em bits
-            rsa.KeySize = 2048;
-
-            // Exporte a chave pública em formato SubjectPublicKeyInfo (SPKI)
-            var publicKeyBytes = rsa.ExportSubjectPublicKeyInfo();
-
-            // Exporte a chave privada em formato PKCS#8
-            var privateKeyBytes = rsa.ExportPkcs8PrivateKey();
-
-            var publicKeyBase64 = Convert.ToBase64String(publicKeyBytes);
-            var privateKeyBase64 = Convert.ToBase64String(privateKeyBytes);
-            return (publicKeyBase64, privateKeyBase64);
         }
     }
 }
